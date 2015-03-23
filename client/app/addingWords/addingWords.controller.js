dcf5d516bd58b25e763ae4278e7ee45e3f8ca210
hauge550 <hauge550@morris.umn.edu>
2015-03-23 15:27:15 -0500

    We we changed "things" to "students" in our addingWords controller. We are working towards making words show up again (as the database was changed in the merge) along with getting students to show up (as that was also changed in the merge)

--- client/app/addingWords/addingWords.controller.js
+++ client/app/addingWords/addingWords.controller.js
@@ -107,61 +107,68 @@ angular.module('WordRiverApp')
     //};
 //=========================================================================
     $scope.students = [];
-    $scope.classList = [];
+  //  $scope.classList = [];
     $scope.studentList = [];
     $scope.studentSortArray = [];
     $scope.filterText = null;
     $rootScope.currentStudent = null;
 
 
-    $http.get('/api/things').success(function(students) {
+    $http.get('/api/student').success(function(students) {
       $scope.students = students;
-      socket.syncUpdates('thing', $scope.students);
+      socket.syncUpdates('student', $scope.students);
       $scope.totalClasses();
       $scope.populateStudentArray();
     });
 
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
+    $scope.addStudent = function() {
+      if($scope.newStudent === '') {
         return;
       }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
+      $http.post('/api/students', { name: $scope.newStudent });
+      $scope.newStudent = '';
     };
 
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
+    $scope.deleteStudent = function(student) {
+      $http.delete('/api/students/' + student._id);
     };
 
     $scope.$on('$destroy', function () {
-      socket.unsyncUpdates('thing');
+      socket.unsyncUpdates('student');
     });
 
-    //creates a list of all classes that exist
-    $scope.totalClasses = function(){
-      for(var i=0; i<$scope.students.length;i++){
-        var found = false;
-        for(var j=0; j<=$scope.classList.length; j++){
-          if($scope.classList[j]==$scope.students[i].class){
-            found = true;
-          }
-        }
-        if (!found){
-          $scope.classList.push($scope.students[i].class);
-        }
-      }
-    };
+    ////creates a list of all classes that exist
+    //$scope.totalClasses = function(){
+    //  for(var i=0; i<$scope.students.length;i++){
+    //    var found = false;
+    //    for(var j=0; j<=$scope.classList.length; j++){
+    //      if($scope.classList[j]==$scope.students[i].class){
+    //        found = true;
+    //      }
+    //    }
+    //    if (!found){
+    //      $scope.classList.push($scope.students[i].class);
+    //    }
+    //  }
+    //};
 
     //creates a list of students
     $scope.populateStudentArray = function(){
       for(var i=0; i<$scope.students.length; i++){
-        for(var j=0; j<$scope.classList.length; j++){
-          if($scope.students[i].class == $scope.classList[j]){
-            var name = $scope.students[i].firstName + " " + $scope.students[i].lastName;
-            $scope.studentList.push({student: name, course: $scope.classList[j]});
-          }
-        }
+        var name = $scope.students[i].firstName + " " + $scope.students[i].lastName;
+        $scope.studentList.push({student: name});
       }
+
+
+
+      //for(var i=0; i<$scope.students.length; i++){
+      //  for(var j=0; j<$scope.classList.length; j++){
+      //    if($scope.students[i].class == $scope.classList[j]){
+      //      var name = $scope.students[i].firstName + " " + $scope.students[i].lastName;
+      //      $scope.studentList.push({student: name, course: $scope.classList[j]});
+      //    }
+      //  }
+      //}
     };
 
     //changes the class view
